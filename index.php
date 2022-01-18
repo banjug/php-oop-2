8c4c9cd7482dab685b5e7d8c88eccfee00debb66
Samuele <miotto.samuele@gmail.com>
2022-01-18 13:41:13 +0100

    edit spacing

--- index.php
+++ index.php
@@ -36,32 +36,25 @@ class Person {
     }
 
     // setter
-
     public function setBirthday($birthday){
         $this->birthday = $birthday;
     }
 
     // getter
-    
     public function getName(){
         return $this->name;
     }
-
     public function getLastname(){
         return $this->lastname;
     }
-
-
     public function getBirthday(){
         return $this->birthday;
     }
 
     // print functions
-
     public function printFullPerson() {
         return $this->getName() . " " . $this->getLastname() . ": " . $this->getBirthday();
     }
-
     public function __toString() {
         return $this->printFullPerson();
     }
@@ -77,47 +70,44 @@ class Employee extends Person{
     }
 
     // setter
-
     public function setHiringDate($hiringDate) {
         $this->hiringDate = $hiringDate;
     }
 
     // getter
-
     public function getSalary() {
         return $this->salary;
     }
-
     public function getHiringDate() {
         return $this->hiringDate;
     }
 
     // print functions
-
     public function printFullEmployee() {
         return $this->getName() . " " . $this->getLastname() . ": " . $this->getSalary() . "€ (" . $this->getHiringDate() . ")";
     }
-
     public function __toString() {
         return $this->printFullEmployee();
     }
 }
 
-echo "<br><br> --------- esercizio get/set -------------<br><br>";
+echo "--------- esercizio get/set -------------<br><br>";
 
-echo " --- class person ---<br>";
-$p1 = new Person("stefano", "carletti");
-$p2 = new Person("giorgia", "franceschini");
-$p1 -> setBirthday("1/1/01");
-$p2 -> setBirthday("2/2/02");
-echo $p1 . "<br>";
-echo $p2 . "<br>";
+echo "--- class person ---<br>";
+    $p1 = new Person("stefano", "carletti");
+    $p1 -> setBirthday("1/1/01");
+    echo $p1 . "<br>";
 
-echo "<br> --- class employee ---<br>";
-$e1 = new Employee("michele", "salvemini", 75000);
-$e2 = new Employee("erika", "busetto", 34000);
-$e1 -> setHiringDate("1/1/01");
-$e2 -> setHiringDate("2/2/02");
-echo $e1 . "<br>";
-echo $e2 . "<br>";
+    $p2 = new Person("giorgia", "franceschini");
+    $p2 -> setBirthday("2/2/02");
+    echo $p2 . "<br>";
+
+echo "<br>--- class employee ---<br>";
+    $e1 = new Employee("michele", "salvemini", 75000);
+    $e1 -> setHiringDate("1/1/01");
+    echo $e1 . "<br>";
+
+    $e2 = new Employee("erika", "busetto", 34000);
+    $e2 -> setHiringDate("2/2/02");
+    echo $e2 . "<br>";
 ?>
